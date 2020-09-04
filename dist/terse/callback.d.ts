e00c6a8ca0f70b425b6c027da775db1d3e6ff307
dikac <rigel234@yahoo.com>
2020-09-05 04:12:38 +0700

    dependency update & bump version

--- dist/terse/callback.d.ts
+++ dist/terse/callback.d.ts
@@ -1,8 +1,8 @@
 import Syslog from "./syslog";
-import Function from "@dikac/t-function/function";
+
 export default class Callback<Arguments extends unknown[] = unknown[]> implements Syslog<Arguments> {
-    callback: Function<[string, ...Arguments], void>;
-    constructor(callback: Function<[string, ...Arguments], void>);
+    callback: (severity:string, ...args:Arguments)=>void;
+    constructor(callback: (severity:string, ...args:Arguments)=>void);
     alert(...argument: Arguments): void;
     crit(...argument: Arguments): void;
     debug(...argument: Arguments): void;
