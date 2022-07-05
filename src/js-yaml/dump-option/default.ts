8d38cc00f0bfb4f1feb59e435871e623cf64c62a
dikac <rigel234@yahoo.com>
2022-07-05 18:16:54 +0700

    feat: add default js yaml dump option

---
+++ src/js-yaml/dump-option/default.ts
@@ -0,0 +1,45 @@
+import {DEFAULT_SCHEMA, DumpOptions} from "js-yaml";
+import util from "util";
+import ObjectType from "@alirya/object/boolean/object";
+import FunctionType from "@alirya/function/boolean/function";
+
+export default function Default<Config extends DumpOptions>(option: Config) : Config {
+
+    const defaults : DumpOptions = {
+        schema : DEFAULT_SCHEMA,
+        quotingType: "'",
+        replacer : (key, value)=>{
+
+            if(Buffer.isBuffer(value)) {
+
+                return `<Buffer length:${value.length}>`
+            }
+
+            if(FunctionType(value)) {
+
+                try {
+
+                    return '<' + util.inspect(value).slice(0,2).slice(0, -1) + '>';
+
+                } catch  {
+
+                    return '<Function>';
+                }
+            }
+
+            if(Array.isArray(value)) {
+
+                return value;
+            }
+
+            if(ObjectType(value)) {
+
+                return Object.assign({}, value);
+            }
+
+            return value
+        }
+    };
+
+    return  Object.assign(defaults, option);
+}
---
+++ src/logform/format/yaml.ts
@@ -0,0 +1,42 @@
+import {Format, TransformableInfo} from "logform";
+import moment from "moment-timezone";
+import TripleBeam from 'triple-beam';
+import Trim from "@alirya/string/trim-parameters";
+import ToYaml from "../../string/to-yaml";
+import {DumpOptions} from "js-yaml";
+
+export type YamlOption = DumpOptions & {
+    timezone : string;
+    format : string
+};
+
+export default class Yaml implements Format {
+
+
+    constructor(public options: YamlOption) {
+
+    }
+
+    transform (transform : TransformableInfo, opts?: any) :  TransformableInfo {
+
+        const date = moment().tz(this.options.timezone).format(this.options.format) + ' ' + transform.level.padEnd(7, ' ');
+        const message = transform.message;
+
+        let meta : any[] = transform[
+            // @ts-ignore
+            Symbol.for('splat')
+        ];
+
+        let body : string = '';
+
+        if(meta) {
+
+            body = ToYaml(meta, this.options);
+        }
+
+        transform[TripleBeam.MESSAGE] = Trim(`${date}: ${message}\n${body}`);
+
+        return transform;
+    }
+}
+
