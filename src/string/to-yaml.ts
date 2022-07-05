cd9da04b012d52312690c9548c636b0490f4e4e2
dikac <rigel234@yahoo.com>
2022-07-05 18:16:10 +0700

    feat: add to yaml converter

---
+++ src/string/to-yaml.ts
@@ -0,0 +1,47 @@
+import {DEFAULT_SCHEMA, dump, DumpOptions} from "js-yaml";
+import * as util from "util";
+import ObjectType from "@alirya/object/boolean/object";
+import FunctionType from "@alirya/function/boolean/function";
+
+export default function ToYaml(data : any, option: DumpOptions = {}) : string {
+
+    const defaults : DumpOptions = {
+        schema : DEFAULT_SCHEMA,
+        quotingType: "'",
+        replacer : (key, value)=>{
+
+            if(Buffer.isBuffer(value)) {
+
+                return `<Buffer length:${value.length}>`
+            }
+
+            if(FunctionType(value)) {
+
+                try {
+
+                    return '<' + util.inspect(value).slice(0,2).slice(0, -1) + '>';
+
+                } catch  {
+
+                    return '<Function>';
+                }
+            }
+
+            if(Array.isArray(value)) {
+
+                return value;
+            }
+
+            if(ObjectType(value)) {
+
+                return Object.assign({}, value);
+            }
+
+            return value
+        }
+    };
+
+    option = Object.assign(defaults, option);
+
+    return dump(data, option)
+}
