b54b7a7f279d7c2765331a85f205ab653e65f32f
dikac <rigel234@yahoo.com>
2023-04-19 08:27:33 +0700

    feat: add symbol

--- src/winston-yaml/replacer/default.ts
+++ src/winston-yaml/replacer/default.ts
@@ -7,6 +7,7 @@ import Array from './array.js';
 import Object from './object.js';
 import CircularReference from './circular-reference.js';
 import Compound from './compound.js';
+import Symbol from "./symbol.js";
 
 export default function Default(
     previous : Replacer[] = [],
@@ -16,6 +17,7 @@ export default function Default(
     return Compound([
         ...previous,
         CircularReference,
+        Symbol,
         Buffer,
         EventEmitter,
         Function,
