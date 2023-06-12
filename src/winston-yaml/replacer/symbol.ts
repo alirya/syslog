9d43a32f6fcdaad3de1a17851a89ddc5bf852618
dikac <rigel234@yahoo.com>
2023-06-13 00:10:41 +0700

    fix: missing module

---
+++ src/winston-yaml/replacer/symbol.ts
@@ -0,0 +1,15 @@
+import Replacer, {ReplacerReturn} from './replacer.js';
+import {IsSymbol} from '@alirya/object/symbol/boolean/symbol.js';
+
+
+export default function  Symbol(value: any, formatted:any[] = [], parent?: Replacer) : ReplacerReturn {
+
+    const valid: boolean = IsSymbol(value);
+
+    if(valid) {
+
+        value = `<Buffer ${value.toString()}>`;
+    }
+
+    return {value, valid};
+}
